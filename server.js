82d7d9e36e55df1f6805b01d5b5eae3a734bf676
Dipal Das <dasdipal8888@gmail.com>
2026-06-29 00:36:42 +0530

    Configure CORS to allow requests from the specified frontend URL

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://task-flow-chi-sooty.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 app.use(morgan("dev"));
 
 app.use("/api/users", require("./routes/userRoutes"));
